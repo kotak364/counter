8e2fea514fa2ddc8af1ddeac76561c66006111c3
kotak364 <75615832+kotak364@users.noreply.github.com>
2021-05-20 16:27:20 -0800

    update

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
+
+//js variable
 const a = document.querySelector("#increment");
 const b = document.querySelector("#decrement");
 const d = document.querySelector("#reset");
-let c = document.querySelector(".number");
+const c = document.querySelector(".number");
 let i = 0;
 
+//increment button event 
+a.addEventListener("click", function () {
+  i += 1;
+  c.textContent = i;
+});
 
-a.addEventListener("click",function () {
- i += 1; 
- c.textContent = i; 
-    
-} 
+// decrement button event 
+b.addEventListener("click", function () {
+  i -= 1;
+  c.textContent = i;
+});
 
-
-)
-
-b.addEventListener("click",function () {
-    i -= 1; 
-    c.textContent = i; 
-       
-   } 
-   
-   
-   )
-
-   d.addEventListener("click",function () {
-    i = 0; 
-    c.textContent = i; 
-       
-   } 
-   
-   
-   )
+// reset button event 
+d.addEventListener("click", function () {
+    i = 0;
+  c.textContent = i;
+});
